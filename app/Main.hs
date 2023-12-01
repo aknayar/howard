f3521c007bb06d050a4d2dadeee30bc0147a1fbc
Akash Nayar <akashknayar5@gmail.com>
2023-11-30 22:17:42 -0500

    airplane

--- app/Main.hs
+++ app/Main.hs
@@ -6,17 +6,16 @@ import qualified Data.Map as Map
 lum = Map.fromList [(0.0, ' '),  (1, '.'),  (2, ','),  (3, '-'),  (4, '~'),  (5, ':'),  (6, ';'),  (7, '='),  (8, '!'),  (9, '*'),  (10, '#'),  (11, '$'),  (12, '@')]
 
 
-convertBoard :: [[Float]] -> [[Char]]
-convertBoard floats = map convertLine floats
-  where
-    convertLine line = map (\x -> case Map.lookup x lum of
-      Nothing -> ' '
-      Just x -> x) line
+convertLine :: [Float] -> [Char]
+convertLine line = map (\x -> case Map.lookup x lum of
+  Nothing -> ' '
+  Just x -> x) line
 
 
-printBoard :: [[Char]] -> IO()
-printBoard board = mapM_ (putStrLn) board
+printBoard :: [[Float]] -> IO()
+printBoard board = mapM_ (putStrLn . convertLine) board
 
 main :: IO ()
 main = do
   putStrLn "hello world"
+  printBoard [[0 / 10, 10 / 10, 100 / 10]]
