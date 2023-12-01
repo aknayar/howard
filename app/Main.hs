96a29b1202feb1ee57cb7b9d0ba0207dd09ea98a
jpeng15 <justinpeng15@gmail.com>
2023-11-30 21:33:18 -0500

    some basic board functions

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,17 @@
 module Main (main) where
 
 import Lib
+import Data.Map
 
 main :: IO ()
-main = someFunc
+main =
+    let " .,-~:;=!*#$@"
+
+convertBoard :: [[Float]] -> [[Char]]
+convertBoard floats = map convertLine float
+  where
+    convertLine line = map (Map.lookup  line
+
+
+printBoard :: [[Char]] -> IO()
+printBoard board = mapM_ (putStrLn) board
