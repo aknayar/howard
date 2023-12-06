bcea9655dc3a9ed19d083356854ff2be2a85d4cb
jpeng15 <justinpeng15@gmail.com>
2023-12-06 17:01:32 -0500

    changed to vec3 data

--- app/Main.hs
+++ app/Main.hs
@@ -2,23 +2,7 @@ module Main (main) where
 
 import Lib
 
-data Vec2I = Vec2I 
-  {
-    xI :: Int,
-    yI :: Int
-  }
-
-data  Vec2D = Vec2D 
-  {
-    xD :: Double, 
-    yD :: Double
-  }
-
-data Ray = Ray
-  { rayOrigin :: Vec2I  -- Assuming you have a Point3D data type
-  , rayDirection :: Vec2D    -- Assuming you have a Vec3 data type
-  , rayPosition :: Vec2I
-  }
+data Vec3 = Vec3 { x :: Double, y :: Double, z :: Double } deriving (Show)
 
 main :: IO ()
 main = do
