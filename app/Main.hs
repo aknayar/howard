0a2d11f4a7e3f87b776c122558747cf66955488b
jpeng15 <justinpeng15@gmail.com>
2023-12-09 14:40:16 -0500

    simple ppm output

--- app/Main.hs
+++ app/Main.hs
@@ -2,11 +2,29 @@ module Main (main) where
 
 import Lib
 
-data Vec3 = Vec3 { x :: Double, y :: Double, z :: Double } deriving (Show)
-
 main :: IO ()
 main = do
-  putStrLn "hello world"
+    -- Image
+    let image_width = 256
+        image_height = 256
+
+    -- Render
+    putStrLn $ "P3\n" ++ show image_width ++ " " ++ show image_height ++ "\n255"
+
+    mapM_ (\j -> mapM_ (\i -> do
+            let r = fromIntegral i / fromIntegral (image_width-1)
+                g = fromIntegral j / fromIntegral (image_height-1)
+                b = 0 :: Double
+
+            let ir = round $ 255.999 * r
+                ig = round $ 255.999 * g
+                ib = round $ 255.999 * b
+
+            putStrLn $ show ir ++ " " ++ show ig ++ " " ++ show ib
+        ) [0..image_width-1]) [0..image_height-1]
+
+
+
 
 
 
---
+++ src/Vec3.hs
@@ -0,0 +1,8 @@
+module Vec3 where
+
+data Vec3 = Vec3
+    {
+        x :: Double,
+        y :: Double,
+        z :: Double
+    } deriving (Show)
