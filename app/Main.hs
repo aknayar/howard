25282193f10ac8633e5842b7db135cddc0f3f46e
jpeng15 <justinpeng15@gmail.com>
2023-12-10 18:56:24 -0500

    new camera class

--- app/Main.hs
+++ app/Main.hs
@@ -7,64 +7,15 @@ import Ray
 import Sphere
 import Hittable
 import Interval
-
+import Camera
 
 
 main :: IO ()
 main = do
-
-    let aspect_ratio = 16.0 / 9.0
-        image_width = 400
-
-        image_height = max 1 (floor $ fromIntegral image_width / aspect_ratio)
-        
-        focal_length = 1.0
-        view_port_height = 2.0
-        view_port_width = view_port_height * (fromIntegral image_width / fromIntegral image_height)
-        camera_center = Vec3 0 0 0
-
+    let
         world = HittableList [Sphere (Vec3 0 0 (-1)) 0.5, Sphere (Vec3 0 (-100.5) (-1)) 100]
 
-    let view_port_u = Vec3 view_port_width 0 0
-        view_port_v = Vec3 0 (-view_port_height) 0
+        cam = initialize (16.0/9.0) 400
 
-        pixel_delta_u = view_port_u `divideVec3`  fromIntegral image_width
-        pixel_delta_v = view_port_v `divideVec3`  fromIntegral image_height
-    
-    let view_port_upper_left = camera_center `minusVec3` Vec3 0 0 focal_length `minusVec3` (view_port_u `divideVec3` 2) `minusVec3` (view_port_v `divideVec3` 2)
-        pixel100_loc = view_port_upper_left `addVec3` ((pixel_delta_u `addVec3` pixel_delta_v) `multiplyVec3` 0.5)
+    render cam world
 
-    putStrLn $ "P3\n" ++ show image_width ++ " " ++ show image_height ++ "\n255"
-
-    mapM_ (\j -> mapM_ (\i -> do
-            let pixel_center = pixel100_loc `addVec3` (pixel_delta_u `multiplyVec3` fromIntegral i) `addVec3` (pixel_delta_v `multiplyVec3` fromIntegral j)
-                ray_direction = pixel_center `minusVec3` camera_center
-
-                r = Ray camera_center ray_direction
-                pixel_color = rayColor r world
-
-            writeColor pixel_color 
-        ) [0..image_width-1]) [0..image_height-1]
-
--- rayColor :: Ray -> Vec3
--- rayColor (Ray org dir)
---                     | t > 0.0 = hit
---                     | otherwise = ret
---                                 where
---                                     t = hitSphere (Vec3 0 0 (-1)) 0.5 (Ray org dir)
---                                     n = unitVector (at (Ray org dir) t `minusVec3` Vec3 0 0 (-1))
---                                     hit = Vec3 (x n + 1) (y n + 1) (z n + 1) `multiplyVec3` 0.5
---                                     unit_direction = unitVector dir
---                                     a = (y unit_direction + 1.0) * 0.5
---                                     ret = (Vec3 1.0 1.0 1.0 `multiplyVec3` (1.0 - a)) `addVec3` (Vec3 0.5 0.7 1.0 `multiplyVec3` a)
-
-rayColor :: Hittable a => Ray -> a -> Vec3
-rayColor (Ray org dir) world = ret
-                where
-                    tempRecord = HitRecord (Vec3 0 0 0 ) (Vec3 0 0 0 ) 0 False
-                    isHit = hit (Ray org dir) (Interval 0 9999999) (Just tempRecord) world
-                    unit_direction = unitVector dir
-                    a = (y unit_direction + 1.0) * 0.5
-                    ret = case isHit of
-                        Nothing -> (Vec3 1.0 1.0 1.0 `multiplyVec3` (1.0 - a)) `addVec3` (Vec3 0.5 0.7 1.0 `multiplyVec3` a)
-                        Just yesHit -> (n yesHit `addVec3` Vec3 1 1 1) `multiplyVec3` 0.5
---
+++ src/Camera.hs
@@ -0,0 +1,60 @@
+module Camera where
+
+import Vec3
+import Ray
+import Hittable
+import Interval
+import Color
+data Camera = Camera 
+    {
+        aspectRatio :: Double,
+        imageWidth :: Int,
+        imageHeight:: Int,
+        center :: Vec3,
+        pixel100Loc :: Vec3,
+        pixelDeltaU :: Vec3,
+        pixelDeltaV :: Vec3
+    } deriving Show
+
+initialize :: Double -> Int -> Camera
+initialize aspect width = Camera aspect width height center pixel100 deltaU deltaV
+                            where
+                                height = max 1 (floor $ fromIntegral width / aspect)
+                                center = Vec3 0 0 0
+                                focalLength = 1.0
+                                viewPortHeight = 2.0
+                                viewPortWidth = viewPortHeight * (fromIntegral width / fromIntegral height)
+
+                                viewPortU = Vec3 viewPortWidth 0 0
+                                viewPortV = Vec3 0 (-viewPortHeight) 0
+
+                                deltaU = viewPortU `divideVec3` fromIntegral width
+                                deltaV = viewPortV `divideVec3` fromIntegral height
+
+                                viewPortUpperLeft = center `minusVec3` Vec3 0 0 focalLength `minusVec3` (viewPortU `divideVec3` 2) `minusVec3` (viewPortV `divideVec3` 2)
+                                pixel100 = viewPortUpperLeft `addVec3` ((deltaU `addVec3` deltaV) `multiplyVec3` 0.5)
+                                
+rayColor :: Hittable a => Ray -> a -> Vec3
+rayColor (Ray org dir) world = ret
+                where
+                    tempRecord = HitRecord (Vec3 0 0 0 ) (Vec3 0 0 0 ) 0 False
+                    isHit = hit (Ray org dir) (Interval 0 9999999) (Just tempRecord) world
+                    unit_direction = unitVector dir
+                    a = (y unit_direction + 1.0) * 0.5
+                    ret = case isHit of
+                        Nothing -> (Vec3 1.0 1.0 1.0 `multiplyVec3` (1.0 - a)) `addVec3` (Vec3 0.5 0.7 1.0 `multiplyVec3` a)
+                        Just yesHit -> (n yesHit `addVec3` Vec3 1 1 1) `multiplyVec3` 0.5
+
+render :: Hittable a => Camera -> a -> IO()
+render cam world = do
+                putStrLn $ "P3\n" ++ show (imageWidth cam) ++ " " ++ show (imageHeight cam) ++ "\n255"
+
+                mapM_ (\j -> mapM_ (\i -> do
+                        let pixel_center = pixel100Loc cam `addVec3` (pixelDeltaU cam`multiplyVec3` fromIntegral i) `addVec3` (pixelDeltaV cam `multiplyVec3` fromIntegral j)
+                            ray_direction = pixel_center `minusVec3` center cam
+
+                            r = Ray (center cam) ray_direction
+                            pixel_color = rayColor r world
+
+                        writeColor pixel_color 
+                    ) [0..imageWidth cam -1]) [0..imageHeight cam-1]
