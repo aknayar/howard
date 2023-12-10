28417f0c7b47c10fa79b6b4a6d261c85ceee2d2d
Akash Nayar <akashknayar5@gmail.com>
2023-12-09 21:57:46 -0500

    progress

---
+++ src/Hittable.hs
@@ -0,0 +1,8 @@
+module Hittable where
+
+import Vec3
+import Ray
+data HitRecord = HitRecord { p :: Vec3, n :: Vec3, t :: Double }
+
+class Hittable a where
+  hit :: Ray -> Double -> Double -> HitRecord -> a -> Bool
--- src/Sphere.hs
+++ src/Sphere.hs
@@ -2,6 +2,37 @@ module Sphere where
 
 import Vec3
 import Ray
+import Hittable
+
+data Sphere = Sphere { center :: Vec3, radius :: Double}
+instance Hittable Sphere where
+    hit r ray_tmin ray_tmax record s = res
+        let
+            oc = origin r `minusVec3` (center s)
+            a = direction r `dot` direction r
+            half_b = oc `dot` direction r
+            c = (oc `dot` oc) - (radius s * radius s)
+            discriminant = half_b*half_b - a*c
+
+        if (discriminant < 0.0)
+            then do return False
+            else return ()
+        let
+            sqrtd = sqrt discriminant
+            root = (-half_b - sqrtd) / a
+        
+        if (root <= ray_tmin || ray_tmax <= root)
+            then do
+                let root = (-half_b + sqrtd) / a
+                if (root <= ray_tmin || ray_tmax <= root)
+                    then do return False
+                    else return ()
+            else return ()
+        -- (t record) <- root
+        -- (p record) <- at r (t record)
+        -- (n record) <- (p record - center) / (radius s) 
+
+        return True
 
 hitSphere :: Vec3 -> Double -> Ray -> Double
 hitSphere center radius ray =
