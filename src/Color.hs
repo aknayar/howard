b6a5a12f2e4b5a01c310620d12dfe726369120ef
jpeng15 <justinpeng15@gmail.com>
2023-12-10 21:56:12 -0500

    updated writeColor function

--- src/Color.hs
+++ src/Color.hs
@@ -1,11 +1,18 @@
 module Color where
 
 import Vec3
+import Interval
 
-writeColor :: Vec3 -> IO()
-writeColor (Vec3 r g b)= do
-                let ir = round $ 255.999 * r
-                    ig = round $ 255.999 * g
-                    ib = round $ 255.999 * b
+writeColor :: Vec3 -> Int -> IO()
+writeColor (Vec3 r g b) samples = do
 
+                let scale = 1.0 / fromIntegral samples
+                    rScaled = r * scale
+                    gScaled = g * scale
+                    bScaled = b * scale
+
+                    range = Interval 0.000 0.999
+                    ir = clamp range r
+                    ig = clamp range g
+                    ib = clamp range b
                 putStrLn $ show ir ++ " " ++ show ig ++ " " ++ show ib
