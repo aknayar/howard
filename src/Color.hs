de756d8e048cb5cb0cc8db1336092475d0927baa
jpeng15 <justinpeng15@gmail.com>
2023-12-10 23:47:55 -0500

    added scaling fix in writeColor

--- src/Color.hs
+++ src/Color.hs
@@ -12,7 +12,8 @@ writeColor (Vec3 r g b) samples = do
                     bScaled = b * scale
 
                     range = Interval 0.000 0.999
-                    ir = clamp range r
-                    ig = clamp range g
-                    ib = clamp range b
+                    ir = 256 * clamp range r
+                    ig = 256 * clamp range g
+                    ib = 256 * clamp range b
+                
                 putStrLn $ show ir ++ " " ++ show ig ++ " " ++ show ib
