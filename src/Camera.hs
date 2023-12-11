64c2dd0a2bcf800aeb7b31cb414f0ec50818f2f6
Akash Nayar <akashknayar5@gmail.com>
2023-12-10 23:33:33 -0500

    builds but does not work

--- src/Camera.hs
+++ src/Camera.hs
@@ -6,6 +6,7 @@ import Hittable
 import Interval
 import Color
 import Utilities
+import System.Random (mkStdGen, StdGen)
 data Camera = Camera 
     {
         aspectRatio :: Double,
@@ -17,6 +18,7 @@ data Camera = Camera
         pixelDeltaU :: Vec3,
         pixelDeltaV :: Vec3
     } deriving Show
+
 
 initialize :: Double -> Int -> Int -> Camera
 initialize aspect width samples = Camera aspect width height samples cent pixel100 deltaU deltaV
@@ -50,9 +52,8 @@ rayColor (Ray org dir) world = ret
 render :: Hittable a => Camera -> a -> IO()
 render cam world = do
                 putStrLn $ "P3\n" ++ show (imageWidth cam) ++ " " ++ show (imageHeight cam) ++ "\n255"
-
                 mapM_ (\j -> mapM_ (\i -> do
-                        let r = getRay cam i j
+                        let r = getRay cam i j (mkStdGen (i * (imageHeight cam-1) + j))
                             pixelColor = updateColor (samplesPerPixel cam) (Vec3 0 0 0) r cam world
                         writeColor pixelColor (samplesPerPixel cam)
                     ) [0..imageWidth cam -1]) [0..imageHeight cam-1]
@@ -63,18 +64,21 @@ updateColor samples cur r cam world = updateColor (samples - 1) next r cam world
                                             where
                                                 next = cur `addVec3` rayColor r world
     
-getRay :: Camera -> Int -> Int -> Ray
-getRay cam i j = Ray org dir
+getRay :: Camera -> Int -> Int -> StdGen -> Ray
+getRay cam i j g = Ray org dir
                 where
-                    pixelCenter = pixel100Loc cam + (pixelDeltaU cam `multiplyVec3` fromIntegral i) + (pixelDeltaV cam `multiplyVec3` fromIntegral j)
-                    pixelSample = pixelCenter + pixelSampleSquare cam
+                    pixelCenter = pixel100Loc cam `addVec3` (pixelDeltaU cam `multiplyVec3` fromIntegral i) `addVec3` (pixelDeltaV cam `multiplyVec3` fromIntegral j)
+                    (pss, g2) = pixelSampleSquare cam g
+                    pixelSample = pixelCenter `addVec3` pss
 
                     org = center cam
                     dir = pixelSample `minusVec3` org
 
-pixelSampleSquare :: Camera -> Vec3
-pixelSampleSquare cam = res
+pixelSampleSquare :: Camera -> StdGen -> (Vec3, StdGen)
+pixelSampleSquare cam g = res
                     where
-                        px = -0.5 + randomDouble
-                        py = -0.5 + randomDouble
-                        res = (pixelDeltaU cam `multiplyVec3` fst px) + (pixelDeltaV cam `multiplyVec3` fst py)
+                        px = -0.5 + d
+                        py = -0.5 + d1
+                        (d, g1) = randomDouble g
+                        (d1,g2) = randomDouble g1
+                        res = ((pixelDeltaU cam `multiplyVec3` px) `addVec3` (pixelDeltaV cam `multiplyVec3` py), g2)
