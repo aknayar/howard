1ce69622e57bb73eb47e5870b18d14b01f93b9c4
Akash Nayar <akashknayar5@gmail.com>
2023-12-11 22:00:41 -0500

    refactor and randomOnHemisphere

--- src/Utilities.hs
+++ src/Utilities.hs
@@ -1,6 +1,5 @@
 module Utilities where
 import System.Random
-import Vec3
 
 degreesToRadians :: Double -> Double
 degreesToRadians deg = deg * pi / 180.0
@@ -12,26 +11,3 @@ randomDoubleR :: Double -> Double -> StdGen -> (Double, StdGen)
 randomDoubleR rand_min rand_max gen =
     let (randValue, newGen) = randomDouble gen
     in (rand_min + (rand_max - rand_min) * randValue, newGen)
-
-randomVec3 :: StdGen -> (Vec3, StdGen)
-randomVec3 = randomVec3R 0.0 1.0
-
-randomVec3R :: Double -> Double -> StdGen -> (Vec3, StdGen)
-randomVec3R rand_min rand_max g =
-    ((Vec3 x y z), g3)
-        where
-            (x, g1) = randomDoubleR rand_min rand_max g
-            (y, g2) = randomDoubleR rand_min rand_max g2
-            (z, g3) = randomDoubleR rand_min rand_max g3
-
-randomInUnitSphere :: StdGen -> (Vec3, StdGen)
-randomInUnitSphere g 
- | lengthSquaredVec3 v <= 1 = (v, g1)
- | otherwise = randomInUnitSphere g1 
-    where
-        (v, g1) = randomVec3 g
-
-randomUnitVector :: StdGen -> (Vec3, StdGen)
-randomUnitVector g = (unitVector v, g1)
-    where
-        (v, g1) = randomInUnitSphere g
