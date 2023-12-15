7fae0ad2a0cb8568b2bb032fb338827f1e837a55
Akash Nayar <akashknayar5@gmail.com>
2023-12-15 01:03:40 -0500

    redner scene

--- app/Main.hs
+++ app/Main.hs
@@ -4,26 +4,65 @@ import Vec3
 import Sphere
 import Hittable
 import Camera
+import Utilities
+import System.Random
+import Data.Maybe (catMaybes)
 
+randomLambert :: Vec3 -> StdGen -> (Sphere, StdGen)
+randomLambert center g = (Sphere center 0.2 mat, g1)
+    where
+        (v, g1) = randomVec3 g
+        mat = Lambertian v
+
+randomMetal :: Vec3 -> StdGen -> (Sphere, StdGen)
+randomMetal center g = (Sphere center 0.2 mat, g2)
+    where
+        (v, g1) = randomVec3 g
+        (f, g2) = randomDouble g1
+        mat = Metal v f
+
+dielectric :: Vec3 -> Sphere
+dielectric center = Sphere center 0.2 (Dielectric 1.5)
+
+randomBall :: Int -> Int -> StdGen -> Maybe Sphere
+randomBall a b g = 
+    if lengthVec3 (center `minusVec3` (Vec3 4 0.2 0)) < 0.9
+            then Nothing
+            else Just sphere
+                where
+                    (chooseMat, g1) = randomDouble g
+                    (x, g2) = randomDouble g1
+                    (y, g3) = randomDouble g2
+                    center = Vec3 (fromIntegral a + 0.9 * x) 0.2 (fromIntegral b + 0.9 * y)
+                    (l, g4) = randomLambert center g3
+                    (m, g5) = randomMetal center g4
+                    sphere = if chooseMat < 0.8
+                        then l
+                        else
+                            if chooseMat < 0.95
+                                then m
+                                else dielectric center
 
 main :: IO ()
 main = do
     let
-        material_ground = Lambertian (Vec3 0.8 0.8 0)
-        material_center = Lambertian (Vec3 0.1 0.2 0.5)
-        material_left = Metal (Vec3 0.8 0.8 0.8) 0.7
-        material_right = Metal (Vec3 0.8 0.6 0.2) 0
+        material_ground = Lambertian (Vec3 0.5 0.5 0.5)
+        groundSphere = Sphere (Vec3 0 (-1000) 0) 1000 material_ground
+        objects = (catMaybes [randomBall a b (mkStdGen (21 * a + b)) | a <- [-11,-10..11], b <- [-11,-10..11]])
 
-        groundSphere = Sphere (Vec3 0 (-100.5) (-1.0)) 100.0 material_ground
-        centerSphere = Sphere (Vec3 0 0 (-1.0)) 0.5 material_center
-        leftSphere = Sphere (Vec3 (-1) 0 (-1.0)) 0.5 material_left
-        rightSphere = Sphere (Vec3 1 0 (-1.0)) 0.5 material_right
+        material1 = Dielectric 1.5
+        material2 = Lambertian (Vec3 0.4 0.2 0.1)
+        material3 = Metal (Vec3 0.7 0.6 0.5) 0.0
 
-        world = HittableList [leftSphere, centerSphere, rightSphere, groundSphere]
+        sphere3 = Sphere (Vec3 0 1 0) 1.0 material1
+        sphere2 = Sphere (Vec3 (-4) 1 0) 1.0 material2
+        sphere1 = Sphere (Vec3 4 1 0) 1.0 material3
+
+        world = HittableList ([sphere1, sphere2, sphere3] ++ objects ++ [groundSphere])
 
         vFov = 20
-        lookFrom = Vec3 (-2) 2 1
-        lookAt = Vec3 0 0 (-1)
+        lookFrom = Vec3 13 2 3
+        lookAt = Vec3 0 0 0
         vUp = Vec3 0 1 0
         cam = initialize (16.0/9.0) 400 100 vFov lookFrom lookAt vUp
 
